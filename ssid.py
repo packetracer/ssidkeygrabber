9a3cf20d23d7e7b7c7c68b6f26887259d1339d47
Ryan <rpere16@gmail.com>
2017-08-10 15:53:42 -0500

    Update ssid.py

--- ssid.py
+++ ssid.py
@@ -1,52 +1,68 @@
+#Load necessary modules
 import subprocess
 import time
 from Tkinter import *
 
-
-
+#Define function that gathers list of profiles from host machine
 def nabProfiles():
+	#Set cmd to netsh profile query shell, pass it to shell
 	cmd = "netsh wlan show profiles"
 	output = subprocess.check_output(cmd)
+	
+	#Split returned output at line breaks
 	output = output.split("\r\n")
+	
+	#init list of profiles, load them into list
 	profileList = []
 	for i in range(0,len(output)):
+		#Check for existence of phrase "Profile" in return line, split off after colon, take right side of split into list
 		if ("Profile" in output[i]):
 			if not ("Profiles" in output[i]):
 				profileName = output[i].split(": ")
 				profileList.append(profileName[1])
+	#Send back the parsed list of profile names
 	return profileList
 
+#Define function that checks wlan profiles based on gathered list of profile names
 def nabKeys(profileList):
+	#init local function variables
 	entry = []
 	bulkout = []
 	WEP = False
 	textout = []
+	#iterate list of profiles, return output of each profile
 	for i in range(0,len(profileList)):
 		cmd = 'netsh wlan show profile name="' + profileList[i] + '" key=clear'
 		output = subprocess.check_output(cmd)
+		#begin parsing out nonsense, starting at line breaks
 		output = output.split("\r\n")
 		for j in range(0,len(output)):
+			#check for SSID name phrase, if so, parse, load into output list
 			if ("SSID name" in output[j]):
 				output[j] = output[j].replace("    ","")
 				textout.append(output[j])
+			#do same for phrase WEP, also, set bool to True to check for WEP key and decode into ASCII
 			if ("WEP" in output[j]):
 				WEP = True
 			if ("Key Content" in output[j]):
 				output[j] = output[j].split(": ")[1]
 				if WEP:
 					WEP = False
-					textout.append("WEP KEY: " + output[j].decode("hex"))
+					textout.append("WEP KEY: \nHex:" + output[j] + "\nASCII: "+output[j].decode("hex"))
 				else:
 					textout.append("WPA KEY: " + output[j])
+			#if no authentication for profile, no key info will exist
 			if ("None" in output[j]):
 				textout.append("No key information for this profile.")
 	return textout
-	
+
+#Define function to collect profile names, keys, and return data
 def gatherInfo():
 	profiles = nabProfiles()
 	textout = nabKeys(profiles)
 	return textout
 
+#send data to TKinter GUI
 def displayInfo(w):
 	textout = gatherInfo()
 	w = Label(root, text= "SSID Key Info:", justify=LEFT, width=30)
@@ -61,7 +77,8 @@ def displayInfo(w):
 			texto = texto + "*************\n"
 	w = Label(root, text = texto,  justify=LEFT, width=30)
 	w.pack()	
-	
+
+#init GUI, make it do stuff
 root = Tk()
 root.title("SSID Key Grabber")
 w = Label(root,text="", anchor="w",    justify=LEFT, width=30)
